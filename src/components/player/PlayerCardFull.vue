28af340bf039063e17f2fc6ee97cfdbc42e363c2
Bartek <bartek.bober.koder@gmail.com>
2020-03-16 20:05:59 +0100

    Revert "remove letter"
    This reverts commit 2b40d3050a73344b9207110f0c13d3c11faf6fdc.

--- src/components/player/PlayerCardFull.vue
+++ src/components/player/PlayerCardFull.vue
@@ -21,7 +21,7 @@
                         <b>Birth:</b> {{$store.state.player.selectedPlayer.dateOfBirth}} ({{playerAge}})
                     </li>
                     <li class="list-group-item d-flex justify-content-between align-items-center" v-if="$store.state.player.selectedPlayer.heightInMeters">
-                        <b>Height:</b> {{$store.state.player.selectedPlayer.heightInMeters}}
+                        <b>Height:</b> {{$store.state.player.selectedPlayer.heightInMeters}}m
                     </li>
                     <li class="list-group-item d-flex justify-content-between align-items-center" v-if="playerNumber != null">
                         <b>Number:</b> {{playerNumber}}
