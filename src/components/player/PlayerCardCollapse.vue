3050c3f833c00205718bad1783baed747ad0a0a5
Bartek <bartek.bober.koder@gmail.com>
2020-03-18 16:04:37 +0100

    refactorize

--- src/components/player/PlayerCardCollapse.vue
+++ src/components/player/PlayerCardCollapse.vue
@@ -3,9 +3,7 @@
     <div class="card">
         <div class="card-header" id="headingOne">
             <h5 class="mb-0">
-                <button class="btn" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    {{title}}
-                </button>
+                {{title}} comments
             </h5>
         </div>
     </div>
