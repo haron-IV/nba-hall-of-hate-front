9e89822b84513db1ab429ee977eb630f334faa32
Bartek <bartek.bober.koder@gmail.com>
2020-03-21 21:02:57 +0100

    add button counter for statistics

---
+++ src/components/player/AddStatisticsButton.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="content">
+    <button class="btn btn-add btn-add--hate">
+        <Icon name="add" />
+    </button>
+    <span class="number">{{hateCount}}</span>
+</div> 
+</template>
+ 
+<script>
+import Icon from "@/components/utility/Icon";
+
+export default {
+name: '',
+data() {
+    return {}
+},
+components: {
+    Icon
+},
+props: {
+    hateCount: { type: Number }
+},
+computed: {},
+watch: {},
+mounted() {},
+created() {},
+methods: {}
+}
+</script>
+
+<style lang='scss'>
+</style>
