fc148d38eccbc5279cb87977a49d10c8641cd050
Bartek <bartek.bober.koder@gmail.com>
2020-03-21 20:16:26 +0100

    remove unused player card component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,11 @@
 <template>
   <article class="mt-3">
-    <!-- <Player-card></Player-card> -->
     <Player-card-full v-if="$store.state.player.playerCardFullView"></Player-card-full>
   </article>
 </template>
 
 <script>
 // @ is an alias to /src
-import PlayerCard from '@/components/player/PlayerCard';
 import PlayerCardFull from '@/components/player/PlayerCardFull';
 
 export default {
