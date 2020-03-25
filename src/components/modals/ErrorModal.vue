3ee4180249f84a4bf7c3ae993f47cc3ec5e7cb76
Bartek <bartek.bober.koder@gmail.com>
2020-03-25 06:07:50 +0100

    add close butto for modal error

--- src/components/modals/ErrorModal.vue
+++ src/components/modals/ErrorModal.vue
@@ -1,29 +1,33 @@
 <template>
  <div class="alert alert-danger modal--error" role="alert" v-if="$store.state.errors.error.isVisible">
     {{$store.state.errors.error.content}}
+    <button class="btn close" @click="$store.commit('hideError')">
+        x
+    </button>
 </div>
 </template>
  
 <script>
-    export default {
-    name: 'Error-modal',
-    data() {
-        return {}
-    },
-    props: {},
-    computed: {},
-    watch: {},
-    mounted() {},
-    created() {
-        this.autoRemoveError();
-    },
-    methods: {
-        autoRemoveError() {
-            setTimeout(() => {
-                this.$store.commit("hideError")    
-            }, 1000 * 15);
-        }
+export default {
+name: 'Error-modal',
+components: { Icon },
+data() {
+    return {}
+},
+props: {},
+computed: {},
+watch: {},
+mounted() {},
+created() {
+    this.autoRemoveError();
+},
+methods: {
+    autoRemoveError() {
+        setTimeout(() => {
+            this.$store.commit("hideError")    
+        }, 1000 * 15);
     }
+}
 }
 </script>
 
